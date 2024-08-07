995da55fcfbe6c4b555d471431c80f5d5f317f5a
akshaya <webdesign@deftinnovations.in>
2024-08-07 12:46:27 +0530

    add navigation animation

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ document.addEventListener('DOMContentLoaded', function() {
         document.body.removeChild(link);
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('ul li a');
+
+    for (const link of links) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    }
+});
