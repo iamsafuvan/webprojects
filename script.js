4ffd3c484d57f21ebdf360d27645bf7cc5ecc99b
akshaya <webdesign@deftinnovations.in>
2024-08-16 12:23:33 +0530

    add nav bar functi

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const downloadBtn = document.getElementById('downloadBtn');
 
-    downloadBtn.addEventListener('click', function() {
+    downloadBtn.addEventListener('click', function () {
         const link = document.createElement('a');
-        link.href = 'docs/safuvanresume.pdf'; 
-        link.download = 'safuvanresume.pdf'; 
+        link.href = 'docs/safuvanresume.pdf';
+        link.download = 'safuvanresume.pdf';
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
     });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const links = document.querySelectorAll('ul li a');
 
     for (const link of links) {
-        link.addEventListener('click', function(e) {
+        link.addEventListener('click', function (e) {
             e.preventDefault();
             const targetId = this.getAttribute('href').substring(1);
             const targetSection = document.getElementById(targetId);
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    
+
 });
 
 
@@ -40,24 +40,21 @@ const closeicon = document.querySelector('.close-icon');
 
 
 // Add click event listener to the menubar
-menubar.addEventListener('click', function(event) {
-  // Toggle 'active' class on boxMenu and overlay
-  boxMenu.classList.toggle('active');
-  overlay.classList.toggle('active');
+menubar.addEventListener('click', function (event) {
+    boxMenu.classList.toggle('active');
+    overlay.classList.toggle('active');
 });
 
 // Add click event listener to the overlay
-overlay.addEventListener('click', function(event) {
-  // Remove 'active' class from boxMenu and overlay
-  boxMenu.classList.remove('active');
-  overlay.classList.remove('active');
-});
-
-closeicon.addEventListener('click', function(event) {
-    // Remove 'active' class from boxMenu and overlay
+overlay.addEventListener('click', function (event) {
     boxMenu.classList.remove('active');
     overlay.classList.remove('active');
-  });
+});
+
+closeicon.addEventListener('click', function (event) {
+    boxMenu.classList.remove('active');
+    overlay.classList.remove('active');
+});
 
 
 
